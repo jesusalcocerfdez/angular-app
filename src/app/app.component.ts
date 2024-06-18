d3b2fd87e646ecfd5374955bbc438bc2f549df44
jesusalcocer <jesusalcocerfdez@gmail.com>
2024-06-18 12:37:33 +0200

    Angular1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';//* importado manualmente
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],//agregado CommondModule manualmente
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hola Mundo con Angular desde Componente';
+  users :string[]=['Pepe','Luis','Maria','Lupe'];
 }
